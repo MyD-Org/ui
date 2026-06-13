7b445b329c02c92c5afad3825ca4f303436a53c6
Emanuel Ponce <manuponce1993@gmail.com>
2026-06-13 13:59:49 -0300

    chore: quitar addon-vitest/@vitest/browser huérfanos del init (resuelve conflicto de peers)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [
     '@chromatic-com/storybook',
-    '@storybook/addon-vitest',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-mcp',
